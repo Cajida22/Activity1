2b7c48f26c46e463afe622578e98ef600f593c45
Shaine Paul Calzo <shainepaul@outlook.com>
2024-09-12 09:18:12 +0800

    Creating new Page

---
+++ Activity1/Pages/UserManagement/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activity1.Pages.UserManagement
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
